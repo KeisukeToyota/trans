eb6fc944faaa28ad739bb353a8441917c858b2e7
KeisukeToyota <hm.pudding0715@gmail.com>
2018-11-11 13:37:53 +0900

    commit

---
+++ install.sh
@@ -0,0 +1,12 @@
+sudo cp -r trans ~/.trans
+
+pip install -r requirements.txt
+
+case `dscl localhost -read Local/Default/Users/$USER UserShell | cut -d' ' -f2 | grep 'zsh'` in
+*bash*)
+echo export PATH='$HOME/.trans/:$PATH' >> ~/.bash_profile;;
+*zsh*)
+echo export PATH='$HOME/.trans/:$PATH' >> ~/.zshrc;;
+*)
+echo 'unknown login shell...';;
+esac
